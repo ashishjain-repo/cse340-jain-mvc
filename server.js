ca8c86643003e51910336186d7b2d57b3adb268f
Ashish Jain <ashishjain99@yahoo.com>
2025-01-31 15:19:40 -0700

    Remove Previous content and added new

--- server.js
+++ server.js
@@ -2,36 +2,41 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
+ 
 // Import all other required modules: Route handlers, Middleware, etc.
-import autoApplyLayout from './src/middleware/layouts.js';
-import homeRoute from './src/routes/home.js';
-
+import baseRoute from './src/routes/index.js';
+import layouts from './src/middleware/layouts.js';
+import staticPaths from './src/middleware/static-paths.js';
+import { notFoundHandler, globalErrorHandler } from './src/middleware/error-handler.js';
+ 
 // Get the current file path and directory name
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+ 
 // Create an instance of an Express application
 const app = express();
 
-// Set EJS as the view engine and the views directory
+// Serve static files from the public directory
+app.use(staticPaths);
+ 
+// Set EJS as the view engine and record the location of the views directory
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src/views'));
-
-// Serve static files from the public directory
-app.use(express.static('public'));
-
-// Apply the layout middleware to automatically wrap views in a layout
-app.use(autoApplyLayout({
-    layoutDir: path.join(__dirname, 'src/views/layouts'),
-    defaultLayout: 'default'
-}));
-
+ 
+// Set Layouts middleware to automatically wrap views in a layout and configure default layout
+app.set('layout default', 'default');
+app.set('layouts', path.join(__dirname, 'src/views/layouts'));
+app.use(layouts);
+ 
 // Use the home route for the root URL
-app.use('/', homeRoute);
+app.use('/', baseRoute);
 
+// Apply error handlers
+app.use(notFoundHandler);
+app.use(globalErrorHandler);
+ 
 // Start the server on the specified port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://127.0.0.1:${PORT}`);
 });
